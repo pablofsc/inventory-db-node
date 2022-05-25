21bc396a5381c333f194c54476e3e8ed5cdaf5b1
pablofsc <pablofsc@outlook.com>
2022-05-25 17:04:18 -0300

    No description

--- src/queries.ts
+++ src/queries.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+require('dotenv').config()
 
 const Pool = require('pg').Pool
 
@@ -8,6 +9,7 @@ const pool = new Pool({
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
+
     ssl: {
         rejectUnauthorized: false,
     }
